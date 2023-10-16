0371177969b7ff6dafb0f8d33eb9fcb92b5bd56b
hiraksaharia <hiraksaharia4@gmail.com>
2023-10-16 22:32:11 +0530

    implmented frontend-useState and useEffect to connect backend and added flask_cors for handling Cross Origin Resource Sharing

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+# cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000/"}})
+cors = CORS(app, resources={r"/api/*": {"origin": "http://localhost:3000/"}})
 
 
 @app.route('/api/data')
 def get_data():
-    data = {'message': 'Hello from Flask'}
+    data = {'message': 'Hello from Flask - API data'}
     return jsonify(data)
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,20 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/data")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md">
+        <h1 className="text-2xl font-semibold mb-4">React And Flask API</h1>
+        <p>{data.message}</p>
+      </div>
     </div>
   );
 }
